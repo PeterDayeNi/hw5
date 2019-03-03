8eff9895058716633b6cfa8cf7d89cfbc97ec00f
PeterDayeNi <47285123+PeterDayeNi@users.noreply.github.com>
2019-03-02 17:30:51 -0800

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,1 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(200 - i, i, 200 + i, i);
+  }
+  for (var a = 10; a <= 390; a = a + 10) {
+    line(a, a + 200, 400 - a, a + 200);
+  }
+}
